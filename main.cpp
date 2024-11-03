8e270c93e0c910c93e6f83cb9e88c5ad51056b52
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-11-02 19:29:55 -0700

    ninth

--- main.cpp
+++ main.cpp
@@ -46,30 +46,30 @@ int main() {
             readS = high_resolution_clock::now();
             lists.push_back(line);
             readE = high_resolution_clock::now();
-            result[0][1] += duration_cast<microseconds>(readE- readS).count();
+            results[0][1][simulations] += duration_cast<microseconds>(readE- readS).count();
             //first action second structure
 
             readS = high_resolution_clock::now();
             sets.insert(line);
             readE = high_resolution_clock::now();
-            result[0][2] += duration_cast<microseconds>(readE - readS).count();
+            results[0][2][simulations] += duration_cast<microseconds>(readE - readS).count();
             //first action third structure
         }
         cout << "Read\t\t" << result[0][0] << "\t" << result[0][1] << "\t" << result[0][2] << endl; 
 
         //sorting string elements-------race 2
         //set is sorted by default
-        result[1][2] = -1;
+        results[1][2][simulations] = -1;
         auto sorts = high_resolution_clock::now();
         lists.sort();
         auto sortend = high_resolution_clock::now();
-        result[1][1] += duration_cast<microseconds>(sortend - sorts).count();
+        results[1][1][simulations] += duration_cast<microseconds>(sortend - sorts).count();
         //second action second structure
 
         sorts = high_resolution_clock::now();
         sort(vect.begin(), vect.end());
         sortend = high_resolution_clock::now();
-        result[1][0] += duration_cast<microseconds>(sortend - sorts).count();
+        results[1][0][simulations] += duration_cast<microseconds>(sortend - sorts).count();
         //second action first structure
         cout << "Sort\t\t" << result[1][0] << "\t" << result[1][1] << "\t" << result[1][2] << endl;
     
@@ -78,7 +78,7 @@ int main() {
         int vectorindex = vect.size() / 2;
         vect.insert(vect.begin() + vectorindex, "TESTCODE");
         auto end = high_resolution_clock::now();
-        result[2][0] += duration_cast<microseconds>(end - start).count();
+        results[2][0][simulations] += duration_cast<microseconds>(end - start).count();
         //third action first structure
 
         start = high_resolution_clock::now();
@@ -87,13 +87,13 @@ int main() {
         advance(i, listindex);
         lists.insert(i, "TESTCODE");
         end = high_resolution_clock::now();
-        result[2][1] += duration_cast<microseconds>(end - start).count();
+        results[2][1][simulations] += duration_cast<microseconds>(end - start).count();
         //third action second structure
 
         start = high_resolution_clock::now();
         sets.insert("TESTCODE");
         end = high_resolution_clock::now();
-        result[2][2] += duration_cast<microseconds>(end - start).count();
+        results[2][2][simulations] += duration_cast<microseconds>(end - start).count();
         //third action third structure
         cout << "Insert\t\t" << result[2][0] << "\t" << result[2][1] << "\t" << result[2][2] << endl;
 
@@ -101,20 +101,28 @@ int main() {
         start = high_resolution_clock::now();
         vect.erase(vect.begin() + vect.size() / 2);
         end = high_resolution_clock::now();
-        result[3][0] += duration_cast<microseconds>(end - start).count();
+        results[3][0][simulations] += duration_cast<microseconds>(end - start).count();
 
         start = high_resolution_clock::now();
         auto j = lists.begin();
         advance(j, listindex);
         lists.erase(j);
         end = high_resolution_clock::now();
-        result[3][1] += duration_cast<microseconds>(end - start).count();
+        results[3][1][simulations] += duration_cast<microseconds>(end - start).count();
 
         start = high_resolution_clock::now();
         sets.erase("TESTCODE");
         end = high_resolution_clock::now();
-        result[3][2] += duration_cast<microseconds>(end - start).count();
+        results[3][2][simulations] += duration_cast<microseconds>(end - start).count();
         cout << "Delete\t\t" << result[3][0] << "\t" << result[3][1] << "\t" << result[3][2] << endl;
+    }
+
+    for (int actions = 0; actions < 4; actions++) {
+        for (int structures = 0; structures < 3; structures++){
+            for (int sim; sim < NUMSIM; sim++){
+                result[actions][structures][]
+            }
+        }
     }
 
     return 0;
