32325e74c6fbf5655fe0e3d954e6682cc7973eb3
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-11-02 18:05:47 -0700

    third

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 //lab 26 ds races pt 2?
+//
 
 #include <iostream>
 #include <set>
@@ -29,17 +30,17 @@ int main() {
         auto readS = high_resolution_clock::now();
         vect.push_back(line);
         auto readE = high_resolution_clock::now();
-        vectdur += duration_cast<milliseconds>(readE - readS).count();
+        vectdur += duration_cast<microseconds>(readE - readS).count();
 
         readS = high_resolution_clock::now();
         lists.push_back(line);
         readE = high_resolution_clock::now();
-        listdur += duration_cast<milliseconds>(readE- readS).count();
+        listdur += duration_cast<microseconds>(readE- readS).count();
 
         readS = high_resolution_clock::now();
         sets.insert(line);
         readE = high_resolution_clock::now();
-        setdur += duration_cast<milliseconds>(readE - readS).count();
+        setdur += duration_cast<microseconds>(readE - readS).count();
     }
     cout << "Read\t\t" << vectdur << "\t" << listdur << "\t" << setdur << endl; //my read is outputting 0 but im not sure why
 
@@ -48,12 +49,12 @@ int main() {
     auto sorts = high_resolution_clock::now();
     lists.sort();
     auto sortend = high_resolution_clock::now();
-    listsort += duration_cast<milliseconds>(sortend - sorts).count();
+    listsort += duration_cast<microseconds>(sortend - sorts).count();
 
     sorts = high_resolution_clock::now();
     sort(vect.begin(), vect.end());
     sortend = high_resolution_clock::now();
-    vecsort += duration_cast<milliseconds>(sortend - sorts).count();
+    vecsort += duration_cast<microseconds>(sortend - sorts).count();
     cout << "Sort\t\t" << vecsort << "\t" << listsort << "\t" << setsort << endl;
     
     //inserting "TESTCODE"--------race 3
@@ -61,7 +62,7 @@ int main() {
     int vectorindex = vect.size() / 2;
     vect.insert(vect.begin() + vectorindex, "TESTCODE");
     auto end = high_resolution_clock::now();
-    vecinsert += duration_cast<milliseconds>(end - start).count();
+    vecinsert += duration_cast<microseconds>(end - start).count();
 
     start = high_resolution_clock::now();
     int listindex = lists.size() / 2;
@@ -69,24 +70,32 @@ int main() {
     advance(i, listindex);
     lists.insert(i, "TESTCODE");
     end = high_resolution_clock::now();
-    listinsert += duration_cast<milliseconds>(end - start).count();
+    listinsert += duration_cast<microseconds>(end - start).count();
 
     start = high_resolution_clock::now();
     sets.insert("TESTCODE");
     end = high_resolution_clock::now();
-    setinsert += duration_cast<milliseconds>(end - start).count();
+    setinsert += duration_cast<microseconds>(end - start).count();
     cout << "Insert\t\t" << vecinsert << "\t" << listinsert << "\t" << setinsert << endl;
 
     //deleting "TESTCODE" ------race 4
     start = high_resolution_clock::now();
     vect.erase(vect.begin() + vect.size() / 2);
     end = high_resolution_clock::now();
-    vecdelete += duration_cast<milliseconds>(end - start).count();
+    vecdelete += duration_cast<microseconds>(end - start).count();
 
     start = high_resolution_clock::now();
+    auto j = lists.begin();
+    advance(j, listindex);
+    lists.erase(j);
+    end = high_resolution_clock::now();
+    listdelete += duration_cast<microseconds>(end - start).count();
 
-
-
+    start = high_resolution_clock::now();
+    sets.erase("TESTCODE");
+    end = high_resolution_clock::now();
+    setdelete += duration_cast<microseconds>(end - start).count();
+    cout << "Delete\t\t" << vecdelete << "\t" << listdelete << "\t" << setdelete << endl;
 
     return 0;
 }
